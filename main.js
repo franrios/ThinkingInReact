e7807f27699a104567b559a4656498e2e9e6535a
franrios <fcojriosbello@gmail.com>
2016-07-04 17:50:31 +0200

    modified data source and ids

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+
 var ProductCategoryRow = React.createClass({
-  render: function() {
+  render: function () {
     return (<tr><th colSpan="2">{this.props.category}</th></tr>);
   }
-});
+})
 
 var ProductRow = React.createClass({
   render: function() {
@@ -12,7 +13,7 @@ var ProductRow = React.createClass({
         {this.props.product.name}
       </span>;
     return (
-      <tr>
+      <tr id="item">
         <td>{name}</td>
         <td>{this.props.product.price}</td>
       </tr>
@@ -35,8 +36,8 @@ var ProductTable = React.createClass({
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Price</th>
+            <th id="np">Name</th>
+            <th id="np">Price</th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
@@ -70,7 +71,6 @@ var FilterableProductTable = React.createClass({
     );
   }
 });
-
 
 var PRODUCTS = [
   {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
